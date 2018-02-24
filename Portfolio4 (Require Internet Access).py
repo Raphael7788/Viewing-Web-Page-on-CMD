25044e667a537c031ca9bc261bc4aa312c5cd7da
Loki <vinomulia@tutanota.com>
2018-02-24 17:19:24 +0700

    Delete Portfolio4 (Require Internet Access).py

--- Portfolio4 (Require Internet Access).py
+++
@@ -1,7 +0,0 @@
-#!usr/bin/env python3
-# -*- coding: UTF-8 -*-
-# Viewing Page Source from a specific website on CMD
-from urllib import urlopen
-
-doc = urlopen("https://www.wired.com/story/why-an-old-theory-of-everything-is-gaining-new-life/").read()
-print doc
