9b034263bcf92af6703bb6b3bfb10652bfc1d409
Loki <vinomulia@tutanota.com>
2018-02-24 17:20:46 +0700

    Update Testing1.py

--- Testing1.py
+++ Testing1.py
@@ -1,5 +1,5 @@
 import urllib.request
-with urllib.request.urlopen("http://www.python.org") as url:
+with urllib.request.urlopen("https://www.wired.com/story/signal-foundation-whatsapp-brian-acton/") as url:
     s = url.read()
 #I'm guessing this would output the html source code?
 print(s)
